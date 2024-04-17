0c0d660f5949d1142694368d2e3c0efad8e47017
PrubelDi <paolarubel@gmail.com>
2024-04-17 20:25:28 +0200

    paola

---
+++ app/src/main/java/com/example/proyecto/Form0.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Form0 extends AppCompatActivity {
+
+}
+
